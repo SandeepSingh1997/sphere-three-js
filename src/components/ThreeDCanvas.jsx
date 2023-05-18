cb7a4c63dfeebc3efcd2ee57e0a106e4438a343e
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-05-18 22:20:49 +0530

    injected glsl code within default react shader

--- src/components/ThreeDCanvas.jsx
+++ src/components/ThreeDCanvas.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+import vertexShader from "./shaders/vertexShader";
+import fragmentShader from "./shaders/fragmentShader";
+
 export default function ThreeDCanvas({ width, height }) {
   //Setup when component has mounted and rendered for the first time
   useEffect(() => {
@@ -9,11 +12,19 @@ export default function ThreeDCanvas({ width, height }) {
 
     const scene = new THREE.Scene();
 
-    const geometry = new THREE.IcosahedronGeometry(5, 50);
+    const geometry = new THREE.IcosahedronGeometry(5, 100);
+
     const material = new THREE.MeshStandardMaterial({
-      color: "#ffffff",
-      roughness: 0.4,
+      onBeforeCompile: (shader) => {
+        console.log(shader.fragmentShader);
+        shader.fragmentShader = shader.fragmentShader.replace(
+          /* glsl */ `#include <color_fragment>`,
+          /* glsl */ `#include <color_fragment>
+          diffuseColor = vec4(1.0, 1.0, 0.0, 1.0); `
+        );
+      },
     });
+
     const sphereMesh = new THREE.Mesh(geometry, material);
     sphereMesh.position.z = 0;
     scene.add(sphereMesh);
@@ -22,18 +33,21 @@ export default function ThreeDCanvas({ width, height }) {
     camera.position.z = 20;
     scene.add(camera);
 
-    const ambientLight = new THREE.AmbientLight("#222222", 1.0);
-    scene.add(ambientLight);
-
-    const pointLight = new THREE.PointLight("#ff0000", 10.0, 60);
+    const pointLight = new THREE.PointLight("#ffffff", 10.0, 60);
     pointLight.position.set(30, 30, 30);
     scene.add(pointLight);
 
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas,
+      alpha: false,
     });
+    renderer.pixelRatio = 2.0;
 
     const controls = new OrbitControls(camera, canvas);
+
+    controls.autoRotate = true;
+    controls.enableDamping = true;
+    controls.autoRotateSpeed = 5.0;
 
     function loop() {
       controls.update();
