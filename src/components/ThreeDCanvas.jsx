0a8ae97e65da930f74966ee4fc2188b57a979cd5
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-05-18 13:56:07 +0530

    added lighting

--- src/components/ThreeDCanvas.jsx
+++ src/components/ThreeDCanvas.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export default function ThreeDCanvas({ width, height }) {
   //Setup when component has mounted and rendered for the first time
   useEffect(() => {
     const canvas = document.getElementById("canvas");
+
     const scene = new THREE.Scene();
 
     const geometry = new THREE.IcosahedronGeometry(5, 50);
@@ -20,14 +22,25 @@ export default function ThreeDCanvas({ width, height }) {
     camera.position.z = 20;
     scene.add(camera);
 
-    const ambientLight = new THREE.AmbientLight("#ffffff");
+    const ambientLight = new THREE.AmbientLight("#222222", 1.0);
     scene.add(ambientLight);
+
+    const pointLight = new THREE.PointLight("#ff0000", 10.0, 60);
+    pointLight.position.set(30, 30, 30);
+    scene.add(pointLight);
 
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas,
     });
 
-    renderer.render(scene, camera);
+    const controls = new OrbitControls(camera, canvas);
+
+    function loop() {
+      controls.update();
+      renderer.render(scene, camera);
+      window.requestAnimationFrame(loop);
+    }
+    loop();
   }, []);
 
   return (
